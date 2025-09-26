443e391e6aa6fdb4577ef280c89e12cc33146df0
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-09-25 22:13:23 -0500

    checkpoint: JS close to completion

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let billInput = document.querySelector(".bill-input");
-let billAmount = Number(billInput.value);
+let billAmount = 0.00;
 let tip5Btn = document.querySelector(".tip-5-btn");
 let tip10Btn = document.querySelector(".tip-10-btn");
 let tip15Btn = document.querySelector(".tip-15-btn");
@@ -11,15 +11,23 @@ let allTipBtns = document.querySelectorAll(".tip-btn");
 let customTipContainer = document.querySelector(".custom-tip-container")
 let customTipInput = document.querySelector(".custom-tip");
 let customTipAmount = Number(customTipInput.value.replace(/\D/g, ''));
+let selectedTip = document.querySelector(".selected-tip");
+let selectedTipAmount = 0.00;
 let peopleInput = document.querySelector(".num-people-input");
-let numPeople = Number(peopleInput.value);
+let numPeople = 0;
 let allInputs = document.querySelectorAll(".inputs");
 let splitTip = document.querySelector(".tip-amount-display");
 let splitTotal = document.querySelector(".total-amount-display");
 
 
-function tipBtnSelect(btn) {
-    return function () {
+billInput.addEventListener("input", function () {
+    billAmount = Number(billInput.value);
+    // console.log(billAmount);
+})
+
+// loop over tip btns for event listener
+for (let btn of allTipBtns) {
+    btn.addEventListener("click", function () {
         // click & reset styling per click
         for (let option of allTipBtns) {
             option.classList.remove("selected-tip");
@@ -28,28 +36,39 @@ function tipBtnSelect(btn) {
         // change style on clicked
         btn.classList.add("selected-tip");
 
+        // update tip amount
+        selectedTipAmount = Number(btn.textContent.replace(/\D/g, ''));
+        // console.log(selectedTipAmount);
+
         // clear custom tip style if needed
         customTipContainer.classList.remove("has-input");
         customTipInput.value = "";
-    };
+    }
+    );
 }
 
-// loop over tip btns
-for (let btn of allTipBtns) {
-    btn.addEventListener("click", tipBtnSelect(btn));
-}
-
-// event listener for custom tip
+// event listener for custom tip input
 customTipInput.addEventListener("input", function () {
     for (let option of allTipBtns) {
         option.classList.remove("selected-tip");
     };
 
     customTipContainer.classList.add("has-input");
+
+    selectedTipAmount = Number(customTipInput.value);
+    console.log(selectedTipAmount);
+
 })
 
-function setDisplays(input) {
-    return function () {
+peopleInput.addEventListener("input", function () {
+    numPeople = Number(peopleInput.value);
+    // console.log(numPeople);
+})
 
-    }
-}
+// for (let input of allInputs) {
+//     input.addEventListener("input", function () {
+//         console.log(`Bill: ${billAmount}`);
+//         console.log(`Tip %: ${selectedTipAmount}`);
+//         console.log(`# People: ${numPeople}`);
+//     });
+// }
